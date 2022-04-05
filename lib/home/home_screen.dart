8c5d437e3ce0830fe235766748431194ef845fb5
Łukasz Jędyk <lukasz.jedyk@gmail.com>
2022-04-06 00:09:45 +0200

    feat: home screen design

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -14,45 +14,95 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: ((context, constraints) {
-        return Scaffold(
-          backgroundColor: Colors.blueGrey.shade800,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                const TopBar(),
-                const SizedBox(height: 40,),
-                ClickbaitButton(
-                  text: 'TAK',
-                  icon: Icons.check,
-                  primaryColor: Colors.green.shade300,
-                  secondaryColor: Colors.green.shade400,
-                  fontSize: 48.0,
-                  onPressed: () {},
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade800,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Column(
+            children: <Widget>[
+              const TopBar(),
+              Expanded(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: constraints.maxWidth < 600 ? 18 : 24,
+                    ),
+                    child: ClickbaitTitle(
+                      title: 'Ogromne sankcje na córki Putina robią wrażenie! Nowe doniesienia amerykańskich mediów.',
+                      fontSize: constraints.maxWidth < 600 ? 24 : 28,
+                    ),
+                  ),
                 ),
-                ClickbaitButton(
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: constraints.maxWidth < 600 ? 24 : 32,
+                  horizontal: constraints.maxWidth < 600 ? 18 : 24,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ClickbaitButton(
+                      text: 'TAK',
+                      icon: Icons.check,
+                      primaryColor: Colors.green.shade300,
+                      secondaryColor: Colors.green.shade400,
+                      fontSize: constraints.maxWidth < 600 ? 36 : 44,
+                      onPressed: () {},
+                    ),
+                    ClickbaitButton(
+                      text: 'NIE',
+                      icon: Icons.close,
+                      primaryColor: Colors.red.shade300,
+                      secondaryColor: Colors.red.shade400,
+                      fontSize: constraints.maxWidth < 600 ? 36 : 44,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: constraints.maxWidth < 600 ? 108 : 144,
+                ),
+                child: ClickbaitButton(
                   text: 'POMIŃ',
                   icon: Icons.skip_next,
                   primaryColor: Colors.lightBlue.shade600,
                   secondaryColor: Colors.lightBlue.shade700,
-                  fontSize: 48.0,
+                  fontSize: constraints.maxWidth < 600 ? 36 : 44,
                   onPressed: () {},
                 ),
-                ClickbaitButton(
-                  text: 'NIE',
-                  icon: Icons.close,
-                  primaryColor: Colors.red.shade300,
-                  secondaryColor: Colors.red.shade400,
-                  fontSize: 48.0,
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        );
-      }),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ClickbaitTitle extends StatelessWidget {
+  final String title;
+  final double fontSize;
+
+  const ClickbaitTitle({
+    Key? key,
+    required this.title,
+    required this.fontSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: fontSize,
+        fontFamily: 'Poppins',
+        fontWeight: FontWeight.w600,
+      ),
     );
   }
 }
